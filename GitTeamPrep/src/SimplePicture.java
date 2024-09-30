2b34c4ab954544eef3d85a4b43f68bb2d24ba774
FaizahM1 <f_muthana@u.pacific.edu>
2024-09-30 15:07:24 -0700

    changed public static int size from 700 to 800

--- GitTeamPrep/src/SimplePicture.java
+++ GitTeamPrep/src/SimplePicture.java
@@ -3,7 +3,7 @@ import acm.graphics.GLabel;
 import acm.program.GraphicsProgram;
 
 public class SimplePicture extends GraphicsProgram {
-	public static int SIZE = 900;
+	public static int SIZE = 800;
 	
 	public void init() {
 		setSize(SIZE, SIZE);
